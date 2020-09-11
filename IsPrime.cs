6e6e09154ca3c3c1a999cfb94d87b863953231da
PawelDutt <71132669+PawelDutt@users.noreply.github.com>
2020-09-11 14:36:39 +0200

    Add files via upload

---
+++ IsPrime.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static bool end = false;
+        static void Main(string[] args)
+        {
+
+            Process.Start("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe", "https://www.youtube.com");
+
+
+            Console.WriteLine("POSZLO");
+        }
+    }
+}
